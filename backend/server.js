4468c4ea2649a88d1962bcfd78c5840b7ab2cd3d
Rahul Singh Meena <149868258+rahul820913@users.noreply.github.com>
2025-04-03 09:00:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const port = process.env.PORT || 3000
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "*",  
+  credentials: true
+}));
 app.use(express.json());
 app.use(bodyParser.json({ limit: "50mb" }));  
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); 
